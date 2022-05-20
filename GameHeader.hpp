6087c8a79e368a6b18bbd3e55e53085c94853b09
VeepDev <alekleosnowy@gmail.com>
2022-05-20 00:27:36 +0000

    The basics, OurMap Class made

--- GameHeader.hpp
+++ GameHeader.hpp
@@ -1,1 +1,20 @@
 //This will be our header file
+
+#include <iostream>
+#include <vector>
+#include <list>
+
+class OurMap{
+public:
+ OurMap();
+ OurMap(unsigned int w, unsigned int h);
+ int d;
+ void PrintMap();
+ void RunGame();
+ 
+private:
+ unsigned int WidthMap;
+ unsigned int HeightMap;
+ std::vector<std::vector<char>> EntireMap;
+ int l;
+};
